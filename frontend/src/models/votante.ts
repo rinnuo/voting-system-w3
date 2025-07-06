82d8db35de10289b830fb4154153ca5247e02d49
Indigo <mateoquinteros7100@gmail.com>
2025-07-05 20:57:30 -0400

    nose, dice que lo modifique yo pero si esta igual a aqui xd

--- frontend/src/models/votante.ts
+++ frontend/src/models/votante.ts
@@ -1,17 +1,17 @@
 export interface Votante {
-	id: number;
-	ci: string;
-	nombreCompleto: string;
-	direccion: string;
-	recintoId: number;
-	fotoCIanverso?: File | null;
-	fotoCIreverso?: File | null;
-	fotoVotante?: File | null;
+  id: number;
+  ci: string;
+  nombreCompleto: string;
+  direccion: string;
+  recintoId: number;
+  fotoCIanverso?: File | null;
+  fotoCIreverso?: File | null;
+  fotoVotante?: File | null;
 }
 
 export interface VotantePublico {
-	ci: string;
-	nombreCompleto: string;
-	recintoId: number;
-	nombreRecinto: string;
+  ci: string;
+  nombreCompleto: string;
+  recintoId: number;
+  nombreRecinto: string;
 }
